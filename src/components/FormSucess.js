3f6cefb2d87d02563169e30f045b1440130c992a
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-08-16 18:45:39 +0100

    Added font awesome icon to success page

--- src/components/FormSucess.js
+++ src/components/FormSucess.js
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 function FormSucess() {
-  return <div>FormSucess</div>
+  return (
+    <div className="container">
+      <h3 className="col-10 mx-auto col-md-8 mt-5 p-2">
+        You have successfully created an account!
+        <FontAwesomeIcon icon={faCheck} />
+      </h3>
+    </div>
+  )
 }
 
 export default FormSucess
